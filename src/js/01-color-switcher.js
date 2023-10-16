a427c246fe35bec096554f3d36f77ce816d30f5b
AlinaStadnyk <135226486+AlinaStadnyk@users.noreply.github.com>
2023-10-16 14:25:58 +0300

    added first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const page = document.querySelector('body');
+let timerId = null;
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    page.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+});
